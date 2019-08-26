2be91de6e17064352de68a9b89ddf35ad34e20ef
Tim Kalan <timkalan99@gmail.com>
2019-08-26 16:08:25 +0200

    Dal vmesnik v povoje, je pretežno nesposoben

--- vmesnik.py
+++ vmesnik.py
@@ -1,5 +1,21 @@
-import bottle
+# import bottle
 import model
 
 """ hahahahaha kaj sploh hahahahahah """
 
+lojtrice = "######################################\n"
+
+def zahtevaj_vnos():
+    return input("Vpišite matriko: ")
+
+def izpis_rezultata(matrika):
+    izpis = lojtrice + "{0}\n".format(matrika)
+    return izpis
+
+def sestej(matrika1, matrika2):
+    
+
+
+
+def pozeni_vmesnik():
+    # to je glavni program, heh
