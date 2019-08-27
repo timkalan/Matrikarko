a24f5cb38b94bf0030902c85af79d9317972be38
Tim Kalan <timkalan99@gmail.com>
2019-08-27 12:31:40 +0200

    Delujoč tekstovni vmesnik, manjka še inverz

--- vmesnik.py
+++ vmesnik.py
@@ -1,11 +1,9 @@
-# import bottle
-#import model
 from model import Matrika
 
-""" hahahahaha kaj sploh hahahahahah """
-
 def pozdrav():
-    print("Ahoj, tu sem, da te rešim dolgočasnih delov linearne algebre!")
+    print("\nAhoj, tu sem, da te rešim dolgočasnih delov linearne algebre!")
+    print("POZOR! Matrike piši tako, da elemente iste vrstice ločiš s presledkom, vrstice pa z vejicami.")
+    print("Identiteta recimo izgleda takole: 1 0 0, 0 1 0, 0 0 1 \n")
 
 def izberi(mozni_odgovori):
     for indeks, odgovor in enumerate(mozni_odgovori):
@@ -14,7 +12,7 @@ def izberi(mozni_odgovori):
     return int(izbira) - 1
 
 def meni():
-    print("Pomagam ti lahko z:")
+    print("Kako ti lahko danes pomagam?")
     izbira = izberi([
         "seštej",
         "odštej",
@@ -44,7 +42,7 @@ def meni():
 
 def sestej():
     # mogoče je še mal tko eh
-    matrika1 = input("Navedi prvo matriko, ki jo želiš sešteti:")
+    matrika1 = input("Navedi prvo matriko, ki jo želiš sešteti: ")
     matrika1 = matrika1.split(",")
     prva_matrika = []
     for vrstica in matrika1:
@@ -53,7 +51,7 @@ def sestej():
         prva_matrika.append(vrstica)
     prva_matrika = Matrika(prva_matrika)
 
-    matrika2 = input("Navedi drugo matriko, ki jo želiš sešteti:")
+    matrika2 = input("Navedi drugo matriko, ki jo želiš sešteti: ")
     matrika2 = matrika2.split(",")
     druga_matrika = []
     for vrstica in matrika2:
@@ -65,33 +63,90 @@ def sestej():
     
 
 def odstej():
-    pass
+    matrika1 = input("Navedi matriko, od katere želiš odštevati: ")
+    matrika1 = matrika1.split(",")
+    prva_matrika = []
+    for vrstica in matrika1:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        prva_matrika.append(vrstica)
+    prva_matrika = Matrika(prva_matrika)
+
+    matrika2 = input("Navedi matriko, ki jo želiš odšteti: ")
+    matrika2 = matrika2.split(",")
+    druga_matrika = []
+    for vrstica in matrika2:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        druga_matrika.append(vrstica)
+    druga_matrika = Matrika(druga_matrika) * -1
+    print(prva_matrika + druga_matrika)
 
 def pomnozi():
-    pass
+    matrika1 = input("Navedi prvo matriko, ki jo želiš množiti: ")
+    matrika1 = matrika1.split(",")
+    prva_matrika = []
+    for vrstica in matrika1:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        prva_matrika.append(vrstica)
+    prva_matrika = Matrika(prva_matrika)
+
+    matrika2 = input("Navedi drugo matriko, ki jo želiš množiti: ")
+    matrika2 = matrika2.split(",")
+    druga_matrika = []
+    for vrstica in matrika2:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        druga_matrika.append(vrstica)
+    druga_matrika = Matrika(druga_matrika)
+    print(prva_matrika * druga_matrika)
 
 def sled():
-    pass
+    matrika = input("Navedi matriko, katere sled želiš izračunati: ")
+    matrika = matrika.split(",")
+    matrika1 = []
+    for vrstica in matrika:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        matrika1.append(vrstica)
+    matrika1 = Matrika(matrika1)
+    print(matrika1.sled())
 
 def transponiraj():
-    pass
+    matrika = input("Navedi matriko, ki jo želiš transponirarti: ")
+    matrika = matrika.split(",")
+    matrika1 = []
+    for vrstica in matrika:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        matrika1.append(vrstica)
+    matrika1 = Matrika(matrika1)
+    print(matrika1.transponiraj())
 
 def determinanta():
-    pass
+    matrika = input("Navedi matriko, katere determinanto želiš izračunati: ")
+    matrika = matrika.split(",")
+    matrika1 = []
+    for vrstica in matrika:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        matrika1.append(vrstica)
+    matrika1 = Matrika(matrika1)
+    print(matrika1.determinanta())
 
 def inverz():
-    pass
-
-def zahtevaj_vnos():
-    return input("Vpišite matriko: ")
-
-def izpis_rezultata(matrika):
-    izpis = "{0}\n".format(matrika)
-    return izpis
-
+    matrika = input("Navedi matriko, ki jo želiš obrniti: ")
+    matrika = matrika.split(",")
+    matrika1 = []
+    for vrstica in matrika:
+        vrstica = vrstica.split()
+        vrstica = [int(x) for x in vrstica]
+        matrika1.append(vrstica)
+    matrika1 = Matrika(matrika1)
+    print(matrika1.inverz())
 
 def main():
-    # to je glavni program, heh
     pozdrav()
     while True:
         meni()
